46618a3f8f50fdb125c6bb2d6f6fa70b8370d68a
minjib <48316078+minjib@users.noreply.github.com>
2019-08-02 20:25:52 +0900

    code

---
+++ FaceQuickstart.py
@@ -0,0 +1,38 @@
+import cognitive_face as CF
+import cv2
+import os
+
+# Replace with a valid subscription key (keeping the quotes in place).
+KEY = '109ed867d2ec4525b21c6084239c232e'
+CF.Key.set(KEY)
+
+# Replace with your regional Base URL
+BASE_URL = 'https://faceapi125.cognitiveservices.azure.com/face/v1.0'
+CF.BaseUrl.set(BASE_URL)
+
+cap = cv2.VideoCapture('video.gif')
+
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+
+while True:
+    ret,frame = cap.read()
+
+    if ret == False:
+        break
+    cv2.imwrite("frame.jpg", frame)
+
+# You can use this example JPG or replace the URL below with your own URL to a JPEG image.
+    img_url = 'frame.jpg'
+    faces = CF.face.detect(img_url, face_id =False, attributes='emotion')
+    maxEmo = 'anger'
+    maxVal = 0
+
+    for face in faces:
+        emotion = face['faceAttributes']['emotion']
+        for emo in face['faceAttributes']['emotion']:
+            val = emotion[emo]
+            if maxVal < val:
+                maxEmo = emo
+        print(maxEmo)
+    os.remove('frame.jpg')
