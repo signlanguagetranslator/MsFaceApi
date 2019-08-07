4b928362fd1190eeb30cd1825b2acc4c65700fc7
minjib <48316078+minjib@users.noreply.github.com>
2019-08-07 15:47:45 +0900

    Update and rename FaceQuickstart.py to FaceEmotion.py

--- FaceEmotion.py
+++ FaceEmotion.py
@@ -1,9 +1,12 @@
 import cognitive_face as CF
 import cv2
 import os
+import numpy as np
+
+
 
 # Replace with a valid subscription key (keeping the quotes in place).
-KEY = 'MyKey'
+KEY = '109ed867d2ec4525b21c6084239c232e'
 CF.Key.set(KEY)
 
 # Replace with your regional Base URL
@@ -14,6 +17,7 @@ cap = cv2.VideoCapture('video.gif')
 
 frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))
+cntEmo = np.zeros(8, dtype=int)
 
 while True:
     ret,frame = cap.read()
@@ -25,14 +29,27 @@ while True:
 # You can use this example JPG or replace the URL below with your own URL to a JPEG image.
     img_url = 'frame.jpg'
     faces = CF.face.detect(img_url, face_id =False, attributes='emotion')
-    maxEmo = 'anger'
+    maxEmo = 0
     maxVal = 0
 
     for face in faces:
         emotion = face['faceAttributes']['emotion']
+        i = 0
         for emo in face['faceAttributes']['emotion']:
             val = emotion[emo]
             if maxVal < val:
-                maxEmo = emo
-        print(maxEmo)
-    os.remove('frame.jpg')
+                maxEmo = i
+                maxVal = val
+            i += 1
+        cntEmo[maxEmo] += 1
+        maxVal = 0
+facelist = ['anger', 'contempt', 'disgust', 'fear', 'happiness', 'neutral', 'sadness', 'surprise']
+maxVal = 0
+res = 'anger'
+i = 0
+for k in cntEmo:
+    if maxVal < k:
+        maxVal = k
+        res = facelist[i]
+    i += 1
+print(res)
